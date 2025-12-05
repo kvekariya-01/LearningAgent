6c6f8e305bfafc9401fc165a286f083e008861f1
karankumar vekariya <karanvekariya90@gmail.com>
2025-12-05 19:49:07 +0530

    Refactor FastAPI app and update response messages

--- AIagent/server.py
+++ AIagent/server.py
@@ -1,16 +1,13 @@
 from fastapi import FastAPI
 from agent.agent import run_agent
 
-app = FastAPI(
-    title="AI Agent System",
-    version="1.0.0"
-)
+app = FastAPI()
 
 @app.get("/")
 def home():
-    return {"message": "AI Agent API Running!"}
+    return {"message": "AI Agent is Running!"}
 
 @app.get("/run")
 def run(task: str):
     result = run_agent(task)
-    return {"input": task, "result": result}
+    return {"task": task, "result": result}
